06d4cd8280811c2eb8b6034cf12bd0808fe17666
m7real <masayeedreal@gmail.com>
2022-10-11 23:32:44 +0600

    created dynamic route

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Topic.css";
 
 const Topic = ({ topic }) => {
   const { id, name, total, logo } = topic;
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate(`topic/${id}`);
+  };
   return (
     <div className="topic">
       <img src={logo} alt="" />
       <h2>{name}</h2>
       <p>Quiz: {total}</p>
+      <button onClick={handleNavigate} className="btn-start">
+        Start Solving
+      </button>
     </div>
   );
 };
