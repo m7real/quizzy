eace90f934c4c67953a4484e2c788d22bfa8f9d4
m7real <masayeedreal@gmail.com>
2022-10-12 14:47:33 +0600

    blog updated

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -7,17 +7,17 @@ const Blog = () => {
     {
       id: 1,
       q: "What is the purpose of React Router?",
-      a: "",
+      a: "React Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route. React Router plays an important role to display multiple views in a single page application. Without React Router, it is not possible to display multiple views in React applications. Most of the social media websites like Facebook, Instagram uses React Router for rendering multiple views.",
     },
     {
       id: 2,
       q: "How does context API work?",
-      a: "",
+      a: "The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to 'prop drilling' or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux. But how does it works? Well, to use context API React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the 'store' and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state",
     },
     {
       id: 3,
       q: "Write about useRef hook.",
-      a: "",
+      a: "The useRef is a hook that allows to directly create a reference to the DOM element in the functional component.It  allows us to persist values between renders. We can use this hook to store a mutable value that does not cause a re-render when updated. If we tried to count how many times our application renders using the useState Hook, we would be caught in an infinite loop since this Hook itself causes a re-render. To avoid this, we can use the useRef Hook. Only one item can be returned from useRef(). That is an Object called current.",
     },
   ];
   return (
